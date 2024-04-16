58ae1a61742c56e8bd74a837d7fe731d7e6587c4
LisaMandarin <chu23001@byui.edu>
2024-04-17 02:40:16 +0800

    notes done

--- js/translate.js
+++ js/translate.js
@@ -20,10 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     outputArea.innerHTML = ""; // clear output area before retrieving new data
+    let resultHtml = '';
 
     for (let sentence of sentences) {
-      await translate(sentence);
+      resultHtml += await translate(sentence);
     }
+    outputArea.innerHTML = resultHtml;
   });
   
   async function translate(sentence) {
@@ -45,16 +47,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // ----- end of api information -----
 
     try {
-    const response = await fetch(url, options);
-    const result = await response.json();
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`HTTP error!  Status: ${response.status}`);
+      }
+      const result = await response.json();
     
-    outputArea.innerHTML += `<span class="original-text">${sentence}</span><br>` + `<span class="translated-text">${result[0].translations[0].text}</span><hr>`;
-
-    // outputArea.innerHTML += sentence + "<br>" + result[0].translations[0].text + "<hr>";
+      return `<span class="original-text">${sentence}</span><br>` + 
+             `<span class="translated-text">${result[0].translations[0].text}</span><hr>`;
     
     } catch (error) {
       console.error("Translation error: ", error);
-      outputArea.innerHTML += `<span class="translate-error">Error translating sentence: ${sentence}</span>`;
+      return `<span class="translate-error">Error translating sentence: ${sentence}</span><br><hr>`;
     }
   }
 });
--- js/wordToChinese.js
+++ js/wordToChinese.js
@@ -1,35 +1,71 @@
 import { qs } from "./util";
 
-const lookUpBtn = qs('#lookUpBtn');
-const notesArea = qs('#notesArea');
-lookUpBtn.addEventListener('click', () => {
-    var selection = document.getSelection();
-    notesArea.innerHTML = selection.toString();
-    // console.log('selected text: ', selection.toString());
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonElem = qs('#lookUpBtn');
+    const outputArea = qs('#notesArea');
+
+    buttonElem.addEventListener('click', async (event) => {
+        event.preventDefault();  // stop the button from submitting
+
+        var selection = document.getSelection().toString();
+        if (selection) {
+            await lookUp(selection);
+        } else {
+            alert("Please select some text to look up.") ;
+        }
+    });
+    
+    async function lookUp(word) {
+        const url = 'https://microsoft-translator-text.p.rapidapi.com/Dictionary/Lookup?to=zh-Hans&api-version=3.0&from=en';
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '1b58f68877msh09e7f1c9031ff15p14b078jsnbd682f41b858',
+                'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+            },
+            body: JSON.stringify([
+                {
+                    Text: word
+                }
+            ])
+        };
+
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const result = await response.json();
+            // outputArea.innerHTML = ''; // clear previous data
+
+            if (result[0].displaySource && result[0].translations.length > 0) {
+                const word = result[0].displaySource;
+                let translations = result[0].translations;
+                
+                const pElem = document.createElement('p');
+                pElem.classList.add('note');
+                pElem.setAttribute('contenteditable', 'true');
+
+                let meanings = [word];
+
+                translations.forEach(translation => {
+                    const meaning = translation.displayTarget;
+                    const partOfSpeech = translation.posTag;
+                    meanings.push(`${meaning} (${partOfSpeech})`);
+                });
+
+                pElem.innerHTML = meanings.join('<br>');
+                outputArea.appendChild(pElem);
+                outputArea.appendChild(document.createElement('hr'));
+                
+            } else {
+                alert('Please select the entire word')
+            }
+        } catch (error) {
+            console.error('Error: ', error.message);
+            outputArea.textContent = "Unable to look up!"
+        }
+    }
 })
 
-
-
-
-// const url = 'https://microsoft-translator-text.p.rapidapi.com/Dictionary/Lookup?to=zh-Hans&api-version=3.0&from=en';
-// const options = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/json',
-// 		'X-RapidAPI-Key': '1b58f68877msh09e7f1c9031ff15p14b078jsnbd682f41b858',
-// 		'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-// 	},
-// 	body: [
-// 		{
-// 			Text: 'mischievous'
-// 		}
-// 	]
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
