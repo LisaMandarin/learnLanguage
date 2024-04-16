280ff2bbe1f79a66c024b1e08906aa7833e25d02
LisaMandarin <chu23001@byui.edu>
2024-04-17 03:45:22 +0800

    first instruction done

--- js/main.js
+++ js/main.js
@@ -20,4 +20,18 @@ document.addEventListener('DOMContentLoaded', function() {
     clearNotesBtn.addEventListener('click', () => notesArea.textContent = "");
     // ----- End of clear the textarea -----
 
+    // ----- Show the hint -----
+    const originalHint = qs('#originalHint');
+    const originalQ = qs('#originalQ');
+
+    originalQ.addEventListener('click', () => {
+        if (originalHint.style.display === 'block') {
+            originalHint.style.display = 'none';
+        } else {
+            originalHint.style.display = 'block';
+        }
+    });
+    // ----- End of show the hint -----
+
+
 });
